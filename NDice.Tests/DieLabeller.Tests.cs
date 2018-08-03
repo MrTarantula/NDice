5fd8ce375693a4a75cba9c434353eaf3a0a8e144
Federico Paolillo <31439756+federico-paolillo@users.noreply.github.com>
2018-08-03 19:20:40 +0200

    Added DieLabeller to enable returning custom values from any IDie implementation

---
+++ NDice.Tests/DieLabeller.Tests.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using NDice;
+using Moq;
+
+namespace NDice.Tests
+{
+	[Trait("Category", "DieLabeller")]
+	public class DieLabellerTests
+	{
+		[Fact]
+		public void DieLabeller_LabelsDieRollProperly()
+		{
+			Dictionary<int, string> labels = new Dictionary<int, string>
+			{
+				{2, "Two" },
+				{3, "Three" }
+			};
+
+			Mock<IDie> dieMock = new Mock<IDie>();
+
+			dieMock.Setup(d => d.Roll()).Returns(2);
+
+			DieLabeller<string> labeller = new DieLabeller<string>(dieMock.Object, labels);
+
+			string actualLabel = labeller.Roll();
+
+			Assert.Equal("Two", actualLabel);
+		}
+
+		[Fact]
+		public void DieLabeller_ReturnsDefaultTLabelValueWhenLabelIsMissing()
+		{
+			Dictionary<int, string> labels = new Dictionary<int, string>();
+
+			Mock<IDie> dieMock = new Mock<IDie>();
+
+			dieMock.Setup(d => d.Roll()).Returns(1);
+
+			DieLabeller<string> labeller = new DieLabeller<string>(dieMock.Object, labels);
+
+			string actualLabel = labeller.Roll();
+
+			Assert.Equal(default(string), actualLabel);
+		}
+
+		[Fact]
+		public void DieLabeller_ThrowaWhenLabelIsMissingIfConfigured()
+		{
+			Dictionary<int, string> labels = new Dictionary<int, string>();
+
+			Mock<IDie> dieMock = new Mock<IDie>();
+
+			dieMock.Setup(d => d.Roll()).Returns(1);
+
+			DieLabeller<string> labeller = new DieLabeller<string>(dieMock.Object, labels, throwsIfLabelMissing: true);
+
+			Func<string> roll = labeller.Roll;
+
+			Assert.Throws<LabelMissingException>(roll);
+		}
+	}
+}
---
+++ NDice/LabelMissingException.cs
@@ -0,0 +1,27 @@
+namespace NDice
+{
+	[System.Serializable]
+	public class LabelMissingException : System.Exception
+	{
+		public LabelMissingException()
+		{
+		}
+
+		public LabelMissingException(string message) 
+			: base(message)
+		{
+		}
+
+		public LabelMissingException(string message, System.Exception inner) 
+			: base(message, inner)
+		{
+		}
+
+		protected LabelMissingException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) 
+			: base(info, context)
+		{
+		}
+	}
+}
