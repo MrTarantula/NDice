d090377e72d1ea042f7cf12b768d38656b49b258
mrtarantula <ryantarantula@gmail.com>
2018-08-13 22:20:45 -0500

    Added some randomizers.

---
+++ NDice.Randomizers.Tests/RandomOrg.Tests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NDice.Randomizers.RandomOrg;
+using NDice.Randomizers.Troschuetz;
+using Xunit;
+
+namespace NDice.Randomizers.Tests
+{
+    public class RandomOrgTests
+    {
+        public static IEnumerable<object[]> Randomizers()
+        {
+            yield return new object[] { new RandomOrgRandomizable("4d80880f-2b77-414e-b0b8-9390499381af") };
+            yield return new object[] { new TroschuetzRandomizable() };
+        }
+
+        [Theory]
+        [MemberData(nameof(Randomizers))]
+        public void RollInRange(IRandomizable rnd)
+        {
+            var die = new Die(rnd, 10);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.InRange(die.Roll(), 0, 10);
+            }
+        }
+    }
+}
