77e347720c5ba2c49edb13a57fa7eada6c026f01
MrTarantula <ryantarantula@gmail.com>
2018-08-31 09:13:53 -0500

    CI fixes.

--- travis.sh
+++ travis.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-version=0.7.1
+version=0.8.0
 
-dotnet pack ./NDice -c Release /p:PackageVersion=$version -o ../build/ndice
-dotnet nuget push ./build/ndice/ -k $nuget -s https://api.nuget.org/v3/index.json
+if [ $TRAVIS_PULL_REQUEST="false" ]
+then
+    dotnet pack ./NDice -c Release /p:PackageVersion=$version -o ../build/ndice
+    dotnet nuget push ./build/ndice/ -k $nugetkey -s https://api.nuget.org/v3/index.json
 
-sleep 9m
+    sleep 9m
 
-dotnet pack ./NDice.Randomizers/NDice.Randomizers.RandomOrg -c Release /p:PackageVersion=$version -o ../../build/rando
-dotnet pack ./NDice.Randomizers/NDice.Randomizers.Troschuetz -c Release /p:PackageVersion=$version -o ../../build/rando
-dotnet pack ./NDice.Randomizers/NDice.Randomizers.SystemCrypto -c Release /p:PackageVersion=$version -o ../../build/rando
+    dotnet pack ./NDice.Randomizers/NDice.Randomizers.RandomOrg -c Release /p:PackageVersion=$version -o ../../build/rando
+    dotnet pack ./NDice.Randomizers/NDice.Randomizers.Troschuetz -c Release /p:PackageVersion=$version -o ../../build/rando
+    dotnet pack ./NDice.Randomizers/NDice.Randomizers.SystemCrypto -c Release /p:PackageVersion=$version -o ../../build/rando
 
-for file in ./build/rando/*.nupkg; do
-    dotnet nuget push $file -k $nuget -s https://api.nuget.org/v3/index.json
-done
+    for file in ./build/rando/*.nupkg; do
+        dotnet nuget push $file -k $nugetkey -s https://api.nuget.org/v3/index.json
+    done
 
-sleep 9m
+    sleep 9m
 
-dotnet pack ./NDice.Randomizers/NDice.Randomizers -c Release /p:PackageVersion=$version -o ../../build/meta
-dotnet nuget push ./build/meta/ -k $nuget -s https://api.nuget.org/v3/index.json
+    dotnet pack ./NDice.Randomizers/NDice.Randomizers -c Release /p:PackageVersion=$version -o ../../build/meta
+    dotnet nuget push ./build/meta/ -k $nugetkey -s https://api.nuget.org/v3/index.json
+fi
 
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=../ndice.coverage.xml ./NDice.Tests --filter Category!=Uniformity
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=../ndice.randomizers.coverage.xml ./NDice.Randomizers.Tests --filter Category!=Uniformity
